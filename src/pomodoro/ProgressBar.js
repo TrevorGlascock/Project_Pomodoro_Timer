efcb98357cce33f4a70cd61eddc7c3d58edf4187
Trevor Glascock <5269652+TrevorGlascock@users.noreply.github.com>
2021-07-23 20:57:26 -0500

    Additional comments for understanding and readability

--- src/pomodoro/ProgressBar.js
+++ src/pomodoro/ProgressBar.js
@@ -1,8 +1,12 @@
 import React from "react";
 
 function ProgressBar({ currentTime, totalTime }) {
+  //Determines the percentage of total progress for the bar
   const progress = (currentTime / totalTime) * 100;
 
+  /****************************************************
+   *************** JSX Return Statement  **************
+   ***************************************************/
   return (
     <div className="progress" style={{ height: "20px" }}>
       <div
